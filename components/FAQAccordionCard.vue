51a2684ba542547e79d5105298fdc72d206e4e15
Rey Garcia <jreygarcia24@gmail.com>
2023-04-21 23:14:49 +0800

    WIP - Added UI for mobile device.

---
+++ components/FAQAccordionCard.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+interface FAQ {
+  question: string;
+  answer: string;
+}
+
+const faqs: FAQ[] = [
+  {
+    question: "How many team members can I invite?",
+    answer:
+      "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
+  },
+  {
+    question: "What is the maximum file upload size?",
+    answer:
+      "No more than 2GB. All files in your account must fit your allotted storage space.",
+  },
+  {
+    question: "How do I reset my password?",
+    answer:
+      "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
+  },
+  {
+    question: "Can I cancel my subscription?",
+    answer:
+      "Yes! Send us a message and we’ll process your request no questions asked.",
+  },
+  {
+    question: "Do you provide additional support?",
+    answer:
+      "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
+  },
+];
+</script>
+
+<template>
+  <div class="FAQAccordionCard">
+    <div class="FAQAccordionCard-container">
+      <div class="FAQAccordionCard-imageContainer">
+        <img
+          srcset="
+            illustration-woman-online-mobile.svg   375w,
+            illustration-woman-online-desktop.svg 1440w
+          "
+          sizes="(max-width: 600px) 100%,
+         100%"
+          src="elva-fairy-800w.jpg"
+          alt="Woman Online"
+        />
+      </div>
+      <div class="FAQAccordionCard-content">
+        <div class="FAQAccordionCard-title">
+          <h2>FAQ</h2>
+        </div>
+        <div class="FAQAccordionCard-accordion">
+          <FAQAccordionItem
+            v-for="faq in faqs"
+            :key="faq.question"
+            :faq="faq"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/FAQAccordionItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="FAQAccordionCard-item">
+    <div class="FAQAccordionCard-accordionHeader">
+      <h3>How many team members can I invite?</h3>
+      <img src="/icon-arrow-down.svg" alt="Arrow Down" />
+    </div>
+    <div class="FAQAccordionCard-accordionBody">
+      <p>
+        You can invite up to 2 additional users on the Free plan. There is no
+        limit on team members for the Premium plan.
+      </p>
+    </div>
+  </div>
+</template>
