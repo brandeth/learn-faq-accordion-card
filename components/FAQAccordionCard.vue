a821805a16a2e06b6db28f7e2d420ed75d7c6fea
Rey Garcia <jreygarcia24@gmail.com>
2023-04-22 16:50:26 +0800

    WIP - align items for accordion item. added patterns for images and shadows.

--- components/FAQAccordionCard.vue
+++ components/FAQAccordionCard.vue
@@ -34,20 +34,32 @@ const faqs: FAQ[] = [
 </script>
 
 <template>
+  <div class="FAQAccordionCard-imageContainer">
+    <div class="WomanAndBox-container">
+      <img
+        srcset="
+          illustration-woman-online-mobile.svg  278w,
+          illustration-woman-online-desktop.svg 472w
+        "
+        sizes="(max-width: 768px) 278px,
+         472px"
+        src="illustration-woman-online-desktop.svg"
+        alt="Woman Online"
+        class="Image-womanOnline"
+      />
+      <img class="Image-box" src="illustration-box-desktop.svg" alt="Box" />
+    </div>
+    <img
+      class="Image-pattern"
+      srcset="bg-pattern-mobile.svg 240w, bg-pattern-desktop.svg 966w"
+      sizes="(max-width: 768px) 240px,
+         966px"
+      src="bg-pattern-mobile.svg"
+      alt="Pattern"
+    />
+  </div>
   <div class="FAQAccordionCard">
     <div class="FAQAccordionCard-container">
-      <div class="FAQAccordionCard-imageContainer">
-        <img
-          srcset="
-            illustration-woman-online-mobile.svg   375w,
-            illustration-woman-online-desktop.svg 1440w
-          "
-          sizes="(max-width: 600px) 100%,
-         100%"
-          src="elva-fairy-800w.jpg"
-          alt="Woman Online"
-        />
-      </div>
       <div class="FAQAccordionCard-content">
         <div class="FAQAccordionCard-title">
           <h2>FAQ</h2>
--- components/FAQAccordionItem.vue
+++ components/FAQAccordionItem.vue
@@ -1,13 +1,23 @@
+<script lang="ts" setup>
+// define props in TypeScript
+
+defineProps<{
+  faq: {
+    question: string;
+    answer: string;
+  };
+}>();
+</script>
+
 <template>
   <div class="FAQAccordionCard-item">
     <div class="FAQAccordionCard-accordionHeader">
-      <h3>How many team members can I invite?</h3>
+      <h3>{{ faq.question }}</h3>
       <img src="/icon-arrow-down.svg" alt="Arrow Down" />
     </div>
     <div class="FAQAccordionCard-accordionBody">
       <p>
-        You can invite up to 2 additional users on the Free plan. There is no
-        limit on team members for the Premium plan.
+        {{ faq.answer }}
       </p>
     </div>
   </div>
