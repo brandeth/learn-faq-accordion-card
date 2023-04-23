be268111334ace31d34b75d0298af223b74ee0dc
Rey Garcia <jreygarcia24@gmail.com>
2023-04-23 10:56:09 +0800

    adjust pattern and added overflow hidden for image container

--- components/FAQAccordionCard.vue
+++ components/FAQAccordionCard.vue
@@ -38,14 +38,28 @@ const faqs: FAQ[] = [
     <div class="FAQAccordionCard-container">
       <div class="FAQAccordionCard-imageContainer">
         <div class="WomanAndBox-container">
-          <img srcset="
+          <img
+            srcset="
               illustration-woman-online-mobile.svg  278w,
               illustration-woman-online-desktop.svg 472w
-            " sizes="(max-width: 768px) 278px,
-             472px" src="illustration-woman-online-desktop.svg" alt="Woman Online" class="Image-womanOnline" />
+            "
+            sizes="(max-width: 768px) 278px,
+             472px"
+            src="illustration-woman-online-desktop.svg"
+            alt="Woman Online"
+            class="Image-womanOnline"
+          />
           <img class="Image-box" src="illustration-box-desktop.svg" alt="Box" />
-          <img class="Image-pattern" srcset="bg-pattern-mobile.svg 240w, bg-pattern-desktop.svg 966w" sizes="(max-width: 768px) 240px,
-             966px" src="bg-pattern-mobile.svg" alt="Pattern" />
+        </div>
+        <div class="Pattern-container">
+          <img
+            class="Image-pattern"
+            srcset="bg-pattern-mobile.svg 240w, bg-pattern-desktop.svg 966w"
+            sizes="(max-width: 768px) 240px,
+             966px"
+            src="bg-pattern-mobile.svg"
+            alt="Pattern"
+          />
         </div>
       </div>
       <div class="FAQAccordionCard-content">
@@ -53,7 +67,11 @@ const faqs: FAQ[] = [
           <h2>FAQ</h2>
         </div>
         <div class="FAQAccordionCard-accordion">
-          <FAQAccordionItem v-for="faq in faqs" :key="faq.question" :faq="faq" />
+          <FAQAccordionItem
+            v-for="faq in faqs"
+            :key="faq.question"
+            :faq="faq"
+          />
         </div>
       </div>
     </div>
