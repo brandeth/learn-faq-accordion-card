473cfca07241380de052da702e37df3c76d8319a
Rey Garcia <jreygarcia24@gmail.com>
2023-04-23 22:51:24 +0800

    fixed image url

--- components/FAQAccordionImage.vue
+++ components/FAQAccordionImage.vue
@@ -6,13 +6,13 @@
           illustration-woman-online-mobile.svg  278w,
           illustration-woman-online-desktop.svg 472w
         " sizes="(max-width: 768px) 278px,
-             472px" src="illustration-woman-online-desktop.svg" alt="Woman Online" class="Image-womanOnline" />
+             472px" src="/illustration-woman-online-desktop.svg" alt="Woman Online" class="Image-womanOnline" />
       </div>
-      <img class="Image-box" src="illustration-box-desktop.svg" alt="Box" />
+      <img class="Image-box" src="/illustration-box-desktop.svg" alt="Box" />
     </div>
     <div class="Pattern-container">
-      <img class="Image-pattern" srcset="bg-pattern-mobile.svg 240w, bg-pattern-desktop.svg 966w" sizes="(max-width: 768px) 240px,
-             966px" src="bg-pattern-mobile.svg" alt="Pattern" />
+      <img class="Image-pattern" srcset="/bg-pattern-mobile.svg 240w, bg-pattern-desktop.svg 966w" sizes="(max-width: 768px) 240px,
+             966px" src="/bg-pattern-mobile.svg" alt="Pattern" />
     </div>
   </div>
 </template>
