0f9b89bd6446c912d3872b30839dfbf8bf1d37c7
Rey Garcia <jreygarcia24@gmail.com>
2023-04-23 22:40:21 +0800

    UI for Desktop

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="Main">
     <FAQAccordionCard />
+    <footer>
+      <div class="attribution">
+        <div>
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>
+        </div> 
+        <div>
+          Coded by <a href="https://github.com/brandeth" target="_blank">Rey Garcia</a>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
